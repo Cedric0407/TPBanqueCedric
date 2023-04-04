e573e93032e60755097001e5fe91eebce1ad4fa1
Cedric <andriamhajanirinacedric@gmail.com>
2023-04-04 15:50:26 +0300

    Déclaration de la source de données

---
+++ src/main/java/mg/cedric/tpbanque/ejb/GestionnaireCompte.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.cedric.tpbanque.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author CEDRIC
+ */
+
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",
+    password="root",
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
+public class GestionnaireCompte {
+    
+}
