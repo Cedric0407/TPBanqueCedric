f3dd4384e8c2918ac641d3226a75f7116da3ad96
Cedric <andriamhajanirinacedric@gmail.com>
2023-04-12 15:20:06 +0300

    Traitement des erreurs et messages de succès dans la page transfert

--- src/main/java/mg/cedric/tpbanque/jsf/Transfert.java
+++ src/main/java/mg/cedric/tpbanque/jsf/Transfert.java
@@ -12,6 +12,7 @@ import jakarta.faces.application.FacesMessage.Severity;
 import jakarta.faces.context.FacesContext;
 import mg.cedric.tpbanque.ejb.GestionnaireCompte;
 import mg.cedric.tpbanque.entities.CompteBancaire;
+import mg.cedric.tpbanque.jsf.util.Util;
 
 /**
  *
@@ -60,12 +61,53 @@ public class Transfert {
     }
     
     public String submitTransfert(){
+        if(idSource == idDestination){
+            Util.messageErreur("Veuillez choisir un autre compte", "", "form:source");
+           return null;
+        }
         CompteBancaire compteSource = compteManager.findById(idSource);
         CompteBancaire compteDestination = compteManager.findById(idDestination);
-        compteManager.transferer(compteSource, compteDestination, montant);
-        return "listeComptes?faces-redirect=true";
         
         
+        Severity severity = FacesMessage.SEVERITY_ERROR;
+        Boolean test = true;
+        if(compteSource == null){
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+           test = false;
+        }
+        if(compteDestination == null){
+           Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+           test = false;
+        }
+        if(montant <= 0){
+           Util.messageErreur("Le montant doit être supérieur à 0", "Le montant doit être supérieur à 0", "form:montant");
+           test = false;
+        }
+        
+        if(test){
+            
+            Severity severityUpdate = null;  
+            String summaryUpdate = "";
+            String messageUpdate = "";
+            try{
+                compteManager.transferer(compteSource, compteDestination, montant);
+                severity = FacesMessage.SEVERITY_INFO;
+                summaryUpdate = "Le transfert de " + compteSource.getNom() + " de " + montant + "€ a été enregistré";
+            }catch(Exception e){
+                severity = FacesMessage.SEVERITY_ERROR;
+                summaryUpdate = "transfer failed";
+                messageUpdate = e.getMessage();
+
+            }finally{
+                FacesMessage facesMessage = new FacesMessage(severity, summaryUpdate, messageUpdate);
+                //FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+                Util.addFlashMessage(facesMessage);
+            }
+            
+            return "listeComptes?faces-redirect=true";
+        }
+        
+        return "";
     }
     
 }
